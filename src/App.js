7372e3a7892cb77074373205fc7ea751f77585bc
Serena Dean <dserena01@yahoo.com>
2022-12-17 16:45:50 -0600

    updated categories menu with categories item components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import './categories.styles.scss';
+import CategoriesMenu from './components/categories-menu/categories-menu.component';
+import Categories from './components/categories-menu/categories.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets'
-    },
-    {
-      id: 3,
-      title: 'Sneakers'
-    },
-    {
-      id: 4,
-      title: 'Womens'
-    },
-    {
-      id: 5,
-      title: 'Men'
-    }
-  ];
-
   return (
-    <div className="categories-container">
-      { categories.map(({title,id}) => 
-       <div className="category-container" key={id}>
-       <div className="background-image"/>
-       <div className="category-body-container">
-           <h2>{title}</h2>
-           <p>Shop Now</p>
-         </div>
-       </div> 
-      )}
-    </div>
+    <Categories categories={CategoriesMenu}/>
   );
 }
 
